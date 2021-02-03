e0b8ad236e04eee6546fd627d1f8c1b9a9e2339e
Ruslan Nagiev <ultraduck100@gmail.com>
2021-02-03 21:13:49 +0300

    fixed transition bug. all animations work fine

--- js/modules/handle-nav-click.js
+++ js/modules/handle-nav-click.js
@@ -3,28 +3,39 @@ const header = document.querySelector('.header');
 const nav = header.querySelector('.navigation');
 const footer = document.querySelector('.footer');
 
-export let didAnimationEnd = true;
+const about = document.getElementById('about');
+const skills = document.getElementById('skills');
+const sections = document.querySelector('.sections-container');
 
 export const handleNavClick = () => {
 
   const trEndHandler = () => {
     header.style.display = 'none';
-    didAnimationEnd = true;
+    about.style.display = 'block';
+    
+    setTimeout(() => {
+      about.style.opacity = '1';
+    });
+
     bgImage.removeEventListener('transitionend', trEndHandler);
   }
 
-  const handleClick = event => {
-    if (event.target.tagName !== 'A' || !didAnimationEnd) {
+  const handleClick = e => {
+    if (e.target.tagName !== 'LI') {
       return;
     }
-    didAnimationEnd = false;
-
     bgImage.classList.add('switch-section-bg-image'); 
     header.classList.add('switch-section');
-    footer.classList.add('switch-section');
-  
+
     bgImage.addEventListener('transitionend', trEndHandler);
   }
 
+  const handleKeydown = e => { 
+    if (e.key === 'Enter') {
+      handleClick(e);
+    }
+  }
+
   nav.addEventListener('click', handleClick);
+  nav.addEventListener('keydown', handleKeydown);
 }
--- js/modules/switch-section.js
+++ js/modules/switch-section.js
@@ -1,21 +1,68 @@
-import { didAnimationEnd } from "./handle-nav-click.js";
-
 const bgImage = document.querySelector('.background > .image');
 const header = document.querySelector('.header');
 const footer = document.querySelector('.footer');
 
-export const switchSection = () => {
+const about = document.getElementById('about');
 
-  document.onkeydown = e => {
-    if (e.key !== 'Escape' || !didAnimationEnd) {
-      return;
-    }; 
+const sectionsContainer = document.querySelector('.sections-container');
+const closeSection = document.querySelector('.close-section');
+
+export const switchSection = () => { let s = null;
+
+  const trEndHandler = e => { 
+    
+    // debugger
+    // const section = e.target.closest('.section');
+    // section.removeEventListener('transitionend', trEndHandler);
+    console.log('vse');
+
+    // return;
+// debugger
+    section.style.display = '';
     header.style.display = '';
-
+  
     setTimeout(() => {
       header.classList.remove('switch-section');
       bgImage.classList.remove('switch-section-bg-image'); 
-      footer.classList.remove('switch-section');
-    }, 0);
+    }, 20);
+    // console.log(new Date - s);
+    
+    console.log(new Date() - s)
+
+    section.addEventListener('transitionend', onOpenHandler);
+    // sectionsContainer.removeEventListener('transitionend', trEndHandler);
+  };
+  
+  const handleClick = e => {
+    console.dir(e.target);
+    s = new Date();
+    // debugger
+
+    section.style.opacity = '0';
+    
+    const duration = parseFloat(getComputedStyle(section)['transitionDuration']) * 1000;
+    setTimeout(trEndHandler, duration);
+    
+    
+    closeSection.removeEventListener('click', handleClick);
+    // sectionsContainer.addEventListener('transitionend', trEndHandler);
   }
+  
+  const onOpenHandler = () => {
+    // debugger
+    console.log('trenderd');
+    closeSection.addEventListener('click', handleClick);
+    section.removeEventListener('transitionend', onOpenHandler);
+  }
+  
+  const handleKeydown = e => { 
+    if (e.key === 'Enter') {
+      handleClick(e);
+    }
+  }
+
+  const section = closeSection.closest('.section');
+  about.addEventListener('transitionend', onOpenHandler);
+  section.addEventListener('keydown', handleKeydown);
+
 }
