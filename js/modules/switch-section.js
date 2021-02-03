aac80a55ac1e2e12b6c82bbedffa4bcbc5830209
Ruslan Nagiev <ultraduck100@gmail.com>
2021-02-03 21:28:35 +0300

    minor code changes

--- js/modules/switch-section.js
+++ js/modules/switch-section.js
@@ -19,33 +19,22 @@ export const switchSection = () => { let s = null;
     }, 20);
     
     console.log(new Date() - s);
-
-    section.addEventListener('transitionend', onOpenHandler);
   };
   
   const handleClick = () => {
     s = new Date();
     section.style.opacity = '0';
-    
     const duration = parseFloat(getComputedStyle(section)['transitionDuration']) * 1000;
     setTimeout(trEndHandler, duration);
-       
-    closeSection.removeEventListener('click', handleClick);
   }
 
-  const onOpenHandler = () => {
-    closeSection.addEventListener('click', handleClick);
-    section.removeEventListener('transitionend', onOpenHandler);
-  }
-  
   const handleKeydown = e => { 
     if (e.key === 'Enter') {
       handleClick(e);
     }
   }
-
+  
   const section = closeSection.closest('.section');
-  about.addEventListener('transitionend', onOpenHandler);
   section.addEventListener('keydown', handleKeydown);
-
+  closeSection.addEventListener('click', handleClick);
 }
