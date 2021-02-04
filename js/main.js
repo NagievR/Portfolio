ab3325d0eebd1d78d40416d31b2dc50eaa55c16b
Ruslan Nagiev <ultraduck100@gmail.com>
2021-02-05 01:29:53 +0300

    minor code changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 import { removeInitialAnimations } from "./modules/remove-initial-animations.js";
 import { sectionOnOpen } from "./modules/section-on-open.js";
 import { sectionOnClose } from "./modules/section-on-close.js";
-
 import { getAllSections } from "./modules/get-all-sections.js";
 
 const sections = {};
+getAllSections(sections, '.section');
+
 const bgImage = document.querySelector('.background > .image');
 const header = document.querySelector('.header');
 const headerInner = header.querySelector('.inner');
@@ -16,6 +17,5 @@ removeInitialAnimations({
   'init-anim-inner-container': innerContainer,
   'init-anim-inner': headerInner,
 });
-getAllSections(sections, '.section');
 sectionOnOpen({ sections, header, bgImage, nav });
 sectionOnClose({ sections, header, bgImage, sectionsContainer });
