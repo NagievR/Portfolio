4148b3b7c8bfc5d87dcd9544019d04c69ce973c2
Ruslan Nagiev <ultraduck100@gmail.com>
2021-02-06 03:20:59 +0300

    the only initial animation left

--- js/main.js
+++ js/main.js
@@ -9,13 +9,9 @@ getAllSections(sections, '.section');
 const bgImage = document.querySelector('.background > .image');
 const header = document.querySelector('.header');
 const headerInner = header.querySelector('.inner');
-const innerContainer = header.querySelector('.inner-container');
 const nav = header.querySelector('.navigation');
 const sectionsContainer = document.querySelector('.sections-container');
 
-removeInitialAnimations({
-  'init-anim-inner-container': innerContainer,
-  'init-anim-inner': headerInner,
-});
-sectionOnOpen({ sections, header, bgImage, nav });
+removeInitialAnimations({ 'init-anim-inner': headerInner });
+sectionOnOpen({ sections, header, bgImage, headerInner, nav });
 sectionOnClose({ sections, header, bgImage, sectionsContainer });
--- js/modules/remove-initial-animations.js
+++ js/modules/remove-initial-animations.js
@@ -1,4 +1,8 @@
+// cancel animations on toggling from display none to its initial value
+
 export const removeInitialAnimations = obj => {
+  // originally this function removed more than one animations.
+  // but I decided not to change this code for future possible additional animations
 
   const elements = Object.values(obj);
   const classNames = Object.keys(obj);
